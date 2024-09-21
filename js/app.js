68b5b571f82b896802996ffc29024e7a2e13f96f
sathishkumar.s <sathishkumar.s@DESKTOP-2F032>
2024-09-21 16:44:47 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+new Swiper('.slider', {
+	// direction: 'vertical',
+	// freeMode: true,
+	speed: 2400,
+	parallax: true,
+	spaceBetween: 1,
+	mousewheel: {
+		enabled: true,
+		sensitivity: 2.4
+	}
+})
